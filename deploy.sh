883c7fd28640cbb68b614106a3f3a9e0e4afc7dc
ydaigo <ydaigo123@gmail.com>
2022-02-13 14:32:08 +0900

    update

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
-npm i
 npm run build
 gcloud builds submit --tag gcr.io/blaze-d9b06/kikuyomu-blog:v1
 gcloud run deploy kikuyomu-blog --image gcr.io/blaze-d9b06/kikuyomu-blog:v1 --region=us-central1
